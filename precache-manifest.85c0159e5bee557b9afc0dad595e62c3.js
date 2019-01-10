56798c723c403c4211c9cb71118a39266f9056ab
Chris Perez <chrisaugustusp@gmail.com>
2019-01-10 15:00:26 -0500

    Updates

---
+++ precache-manifest.85c0159e5bee557b9afc0dad595e62c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca3a3759d1b92fe1b431",
+    "url": "/chrisauinmotion/DigiLib.git/static/js/runtime~main.ca3a3759.js"
+  },
+  {
+    "revision": "053fa8c83a96cdd669ec",
+    "url": "/chrisauinmotion/DigiLib.git/static/js/main.053fa8c8.chunk.js"
+  },
+  {
+    "revision": "499bda9e7d37f13fdd52",
+    "url": "/chrisauinmotion/DigiLib.git/static/js/1.499bda9e.chunk.js"
+  },
+  {
+    "revision": "a6009b217cc368c671b0d39f2b898217",
+    "url": "/chrisauinmotion/DigiLib.git/index.html"
+  }
+];
